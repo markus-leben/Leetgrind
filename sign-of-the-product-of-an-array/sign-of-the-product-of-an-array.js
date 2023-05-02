7bea1e5f726832434335a96d14122e695590dcdb
Markus Leben <61570879+markus-leben@users.noreply.github.com>
2023-05-02 10:22:24 -0700

    Time: 61 ms (58.48%) | Memory: 44.5 MB (9.00%) - LeetSync

--- sign-of-the-product-of-an-array/sign-of-the-product-of-an-array.js
+++ sign-of-the-product-of-an-array/sign-of-the-product-of-an-array.js
@@ -5,10 +5,12 @@
 var arraySign = function(nums) {
     var n = false
     for (let i of nums) {
-        if (i === 0) return 0;
-        
-        if (i < 0) n = !n;
-        
+        if (i === 0) {
+            return 0
+        } 
+        if (i < 0) {
+            n = !n
+        }
     }
     return n? -1 : 1;
     
