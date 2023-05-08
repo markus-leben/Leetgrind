db4e2fc5c0a7f8cb620ff9294779f066934820bd
Markus Leben <61570879+markus-leben@users.noreply.github.com>
2023-05-08 14:37:11 -0600

    Time: 64 ms (41.10%) | Memory: 44 MB (50.00%) - LeetSync

--- matrix-diagonal-sum/matrix-diagonal-sum.js
+++ matrix-diagonal-sum/matrix-diagonal-sum.js
@@ -4,12 +4,14 @@
  */
 var diagonalSum = function(mat) {
     var count = 0
-    for (let i = 0; i < mat[0].length; i++) {
+    for (let i = 0; i < (mat[0].length)/2; i++) {
         if (i === (mat[0].length-1)/2) {
             count += mat[i][i]
         } else {
             count += mat[i][i]
             count += mat[(mat[0].length-1)-i][i]
+            count += mat[i][(mat[0].length-1)-i]
+            count += mat[(mat[0].length-1)-i][(mat[0].length-1)-i]
         }
     }
     return count
